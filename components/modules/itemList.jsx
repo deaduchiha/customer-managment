24d7f6df6494c9212ba15695de0b524929f0e42d
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-21 03:28:30 +0330

    handle add items for purchase product

--- components/modules/itemList.jsx
+++ components/modules/itemList.jsx
@@ -3,10 +3,19 @@ import React from "react";
 
 const ItemList = ({ form, setForm }) => {
   const { products } = form;
+
+  const addHandler = () => {
+    setForm({
+      ...form,
+      products: [...products, { name: "", price: "", qty: "" }],
+    });
+  };
   return (
     <Box>
       <Text>Purchase products</Text>
-      <Button colorScheme="green">Add Item</Button>
+      <Button colorScheme="green" onClick={addHandler}>
+        Add Item
+      </Button>
     </Box>
   );
 };
