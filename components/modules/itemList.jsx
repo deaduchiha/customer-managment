418a5104aeaa44d4d87b3fe2daae48793a9513de
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-21 03:26:24 +0330

    de structure products from form and create a box for adding

--- components/modules/itemList.jsx
+++ components/modules/itemList.jsx
@@ -1,7 +1,14 @@
+import { Box, Button, Text } from "@chakra-ui/react";
 import React from "react";
 
-const ItemList = () => {
-  return <h1>item list</h1>;
+const ItemList = ({ form, setForm }) => {
+  const { products } = form;
+  return (
+    <Box>
+      <Text>Purchase products</Text>
+      <Button colorScheme="green">Add Item</Button>
+    </Box>
+  );
 };
 
 export default ItemList;
