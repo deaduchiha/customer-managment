03329a291849ed70a03fa6936f724f6882ee0e62
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-20 04:43:30 +0330

    folder structure and fix something about chakra ui

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
+};
+
+export default Home;
