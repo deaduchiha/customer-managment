5ffb36db30155423e743051980521549e0c560f9
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-21 17:50:47 +0330

    handle errors for edit api

--- pages/api/edit/[customerId].js
+++ pages/api/edit/[customerId].js
@@ -1,3 +1,15 @@
-const handler = async (req, res) => {};
+import connectDB from "@/utils/connectDB";
+
+const handler = async (req, res) => {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "failed",
+      message: "error in connecting to DB",
+    });
+  }
+};
 
 export default handler;
