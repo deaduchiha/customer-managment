5a6c34c63e8f988fcce01d9130d54a4a8ac35822
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-21 03:31:25 +0330

    change formInput to itemList on form and take props

--- components/modules/form.jsx
+++ components/modules/form.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import FormInput from "./formInput";
+import ItemList from "./itemList";
 
-const Form = () => {
-  return <FormInput />;
+const Form = ({ form, setForm }) => {
+  return <ItemList form={form} setForm={setForm} />;
 };
 
 export default Form;
