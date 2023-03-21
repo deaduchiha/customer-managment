3a02d483aae6eaddd7f2531fd7e00f553447c270
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-21 13:46:47 +0330

    develope form and add formInputs

--- components/modules/form.jsx
+++ components/modules/form.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+
+// components
 import FormInput from "./formInput";
 import ItemList from "./itemList";
 
 const Form = ({ form, setForm }) => {
-  return <ItemList form={form} setForm={setForm} />;
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  return (
+    <Box>
+      {[
+        "firstName",
+        "lastName",
+        "email",
+        "phone",
+        "address",
+        "postalCode",
+        "date",
+      ].map((fieldName, index) => (
+        <FormInput
+          key={index}
+          name={fieldName}
+          label={fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}
+          type="text"
+          value={form[fieldName]}
+          onChange={changeHandler}
+        />
+      ))}
+      <ItemList {...{ form, setForm }} />
+    </Box>
+  );
 };
 
 export default Form;
