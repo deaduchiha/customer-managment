ab805612ee8998cb3dd6a3708c9fee44a4db6286
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-21 18:01:55 +0330

    develope PATH method and handle errors

--- pages/api/edit/[customerId].js
+++ pages/api/edit/[customerId].js
@@ -1,3 +1,4 @@
+import Customer from "@/models/Customer";
 import connectDB from "@/utils/connectDB";
 
 const handler = async (req, res) => {
@@ -9,6 +10,39 @@ const handler = async (req, res) => {
       status: "failed",
       message: "error in connecting to DB",
     });
+    return;
+  }
+
+  if (req.method === "PATCH") {
+    const id = req.query.customerId; //take id from query
+    const data = req.body.data; //take data from body (front)
+
+    try {
+      const customer = await Customer.findOne({ _id: id }); //find the one have same id as _id
+      //   change all data
+      customer.firstName = data.firstName;
+      customer.lastName = data.lastName;
+      customer.email = data.email;
+      customer.phone = data.phone;
+      customer.address = data.address;
+      customer.postalCode = data.postalCode;
+      customer.date = data.date;
+      customer.products = data.products;
+      customer.updatedAt = Date.now();
+
+      customer.save();
+
+      res.status(200).json({
+        status: "success",
+        data: customer,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        status: "failed",
+        message: "Error to updating data",
+      });
+    }
   }
 };
 
