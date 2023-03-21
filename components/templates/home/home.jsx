bc67898454b7a43f9802d76b4a38af6802705650
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-21 15:03:26 +0330

    create SSR landing page and create homepage template

---
+++ components/templates/home/home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = ({ customers }) => {
+  return <h1>homepage</h1>;
+};
+
+export default HomePage;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
 
-const Home = () => {
-  return (
-    <>
-      <Heading as="h2">hello</Heading>
-    </>
-  );
+import connectDB from "@/utils/connectDB";
+import Customer from "@/models/Customer";
+
+import HomePage from "@/components/templates/home/home";
+
+const Home = ({ customers }) => {
+  return <HomePage customers={customers} />;
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  try {
+    await connectDB();
+    const customers = await Customer.find();
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customers)),
+      },
+    };
+  } catch (err) {
+    return {
+      notFound: true,
+    };
+  }
+};
