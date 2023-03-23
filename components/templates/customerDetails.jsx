2b2ad3d84f6235821b008d5d8037e80f3a813d64
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-23 16:06:17 +0430

    create customer details template and handle customer details page

---
+++ components/templates/customerDetails.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const CustomerDetails = () => {
+  return <h1>this is customerrrrr</h1>;
+};
+export default CustomerDetails;
--- pages/customer/[customerId].jsx
+++ pages/customer/[customerId].jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import CustomerDetails from "@/components/templates/customerDetails";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
-const Index = () => {};
+const Index = () => {
+  const [data, setData] = useState(null);
+  const router = useRouter();
+
+  const {
+    query: { customerId },
+  } = router;
+
+  useEffect(() => {
+    fetch(`/api/customer/${customerId}`)
+      .then((res) => res.json())
+      .then((data) => setData(data.data));
+  });
+
+  if (data) return <CustomerDetails />;
+};
 
 export default Index;
